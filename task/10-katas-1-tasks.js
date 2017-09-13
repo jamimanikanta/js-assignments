670dc85e74eccd4b06d8e736131e1c0dadea7995
jamimanikanta <jamimanikanta369@gmail.com>
2017-09-13 15:37:26 +0530

    Update 10-katas-1-tasks.js

--- task/10-katas-1-tasks.js
+++ task/10-katas-1-tasks.js
@@ -17,8 +17,56 @@
  *  ]
  */
 function createCompassPoints() {
-    throw new Error('Not implemented');
-    var sides = ['N','E','S','W'];  // use array of cardinal directions only!
+    
+    const sides = ['N','E','S','W'],  // use array of cardinal directions only!
+          result = [];
+    
+    for (let i = 0; i < 4; i++) {
+        const from = i % 2 ? 2 : 5;
+        for (let j = -from; j <= from; j++) {
+            const pos = (i * 8 + j + 32) % 32, // current position in result
+                  posi = (i + (j < 0 ? -1 : 1) + 4) % 4, // previous or next position in sides
+                  ja = Math.abs(j);
+            result[pos] = sides[i];
+            if (j) {
+                /*switch (ja) {
+                    case 1:
+                    case 3:
+                    case 5:
+                        result[pos] +=
+                            (ja > 1 ? sides[posi] : '') +
+                            'b' +
+                            (ja === 3 ? sides[i] : sides[posi]);
+                        break;
+                    case 2:
+                        result[pos] += i % 2 ? sides[posi] + sides[i] : sides[i] + sides[posi];
+                        break;
+                    case 4:
+                        result[pos] += sides[posi];
+                        break;
+                }*/
+                result[pos] +=
+                    ja % 2 ?
+                        (
+                            (ja > 1 ? sides[posi] : '') +
+                            'b' +
+                            (ja === 3 ? sides[i] : sides[posi])
+                        ) :
+                        (
+                            ja === 4 ?
+                            sides[posi] :
+                            (i % 2 ? sides[posi] + sides[i] : sides[i] + sides[posi])
+                                
+                        );
+            }
+        }
+    }
+    return result.map((v, i) => {
+        return {
+            abbreviation: v,
+            azimuth: i * 11.25
+        };
+    });
 }
 
 
@@ -56,7 +104,19 @@ function createCompassPoints() {
  *   'nothing to do' => 'nothing to do'
  */
 function* expandBraces(str) {
-    throw new Error('Not implemented');
+    const queue = [str],
+          exist = [];
+    while (queue.length > 0) {
+        str = queue.shift();
+        let match = str.match(/\{([^{}]+)\}/);
+        if (match) {
+            for (let value of match[1].split(','))
+                queue.push(str.replace(match[0], value));
+        } else if (exist.indexOf(str) < 0) {
+            exist.push(str);
+            yield str;
+        }
+    }
 }
 
 
@@ -88,7 +148,24 @@ function* expandBraces(str) {
  *
  */
 function getZigZagMatrix(n) {
-    throw new Error('Not implemented');
+    let row = 0,
+        col = 0,
+        direction = 0,
+        arr = Array.from({length: n}, () => Array.from({length: n}));
+    for (let i = 0; i < n * n; i++) {
+        arr[row][col] = i;
+        
+        if (direction % 2) {
+            row += direction % 4 === 1 ? 1 : -1;
+            col += direction % 4 === 1 ? -1 : 1;
+        } else if ((direction % 4 === 0 && col < n - 1) || row === n - 1)
+            col++;
+        else
+            row++;
+        if (direction % 2 === 0 || row === 0 || row === n - 1 || col === 0 || col === n - 1)
+            direction++;
+    }
+    return arr;
 }
 
 
@@ -113,7 +190,66 @@ function getZigZagMatrix(n) {
  *
  */
 function canDominoesMakeRow(dominoes) {
-    throw new Error('Not implemented');
+    
+    // убираем все дубли и подсчитываем кол-во вхождений каждой из цифр
+    let res = false,
+        cnt = Array.from({length: 7}, () => 0);
+    dominoes = dominoes.filter((v, i) => {
+        if (v[0] !== v[1]) {
+            cnt[v[0]]++;
+            cnt[v[1]]++;
+            return true;
+        } else {
+            if (!dominoes.some((v1, i1) => i !== i1 && (v[0] === v1[0] || v[0] === v1[1])))
+                res = true;
+            return false;
+        }
+    });
+    
+    // если некуда пристроить дубль
+    if (res)
+        return false;
+    
+    if (dominoes.length === 1)
+        return true;
+    
+    // если нечётных количеств цифр больше 2, то домино сложить нельзя
+    res = 0;
+    for (let i = 0; i < 7; i++)
+        if (cnt[i] % 2)
+            res++;
+    if (res > 2)
+        return false;
+    
+    // комбинируем 2 кости и получаем все возможные варианты
+    function* combine(domino1, domino2) {
+        for (let i = 0; i < 2; i++)
+            for (let j = 0; j < 2; j++)
+                if (domino1[i] === domino2[j])
+                    yield [domino1[1 - i], domino2[1 - j]];
+    }
+    
+    const exclude = Array.from({length: dominoes.length}, (v, i) => i);
+    let excludeCount = 1;
+    
+    // складываем кости всеми возможными путями
+    function rec(domino) {
+        if (excludeCount === dominoes.length)
+            return true;
+        for (let i = 1; i < dominoes.length; i++)
+            if (exclude[i]) {
+                exclude[i] = false;
+                excludeCount++;
+                for (let v of combine(domino, dominoes[i]))
+                    if (rec(v))
+                        return true;
+                exclude[i] = true;
+                excludeCount--;
+            }
+        if (excludeCount === 1)
+            return false;
+    }
+    return rec(dominoes[0]);
 }
 
 
@@ -137,7 +273,23 @@ function canDominoesMakeRow(dominoes) {
  * [ 1, 2, 4, 5]          => '1,2,4,5'
  */
 function extractRanges(nums) {
-    throw new Error('Not implemented');
+    let arr = [[nums[0]]];
+    
+    // группируем блоки
+    for (let i = 1; i < nums.length; i++)
+        if (nums[i] - 1 === arr[arr.length - 1][arr[arr.length - 1].length - 1])
+            arr[arr.length - 1].push(nums[i]);
+        else
+            arr.push([nums[i]]);
+    
+    // переводим блоки в строки
+    for (let i in arr)
+        if (arr[i].length > 2)
+            arr[i] = arr[i][0] + '-' + arr[i][arr[i].length - 1];
+        else
+            arr[i] = arr[i].join(',');
+    
+    return arr.join(',');
 }
 
 module.exports = {
